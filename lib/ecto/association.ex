f057250df56fafe49170d71520bff9e558be6e84
Xiaohui Gan <286390860@qq.com>
2019-06-17 14:45:54 +0800

    only do after compile in dev mode of erlang

--- lib/ecto/association.ex
+++ lib/ecto/association.ex
@@ -522,17 +522,19 @@ defmodule Ecto.Association.Has do
 
   @doc false
   def after_compile_validation(%{queryable: queryable, related_key: related_key}, env) do
-    cond do
-      not is_atom(queryable) or queryable in env.context_modules ->
-        :ok
-      not Code.ensure_compiled?(queryable) and :code.get_mode() != :embedded ->
-        {:error, "associated schema #{inspect queryable} does not exist"}
-      not function_exported?(queryable, :__schema__, 2) ->
-        {:error, "associated module #{inspect queryable} is not an Ecto schema"}
-      is_nil queryable.__schema__(:type, related_key) ->
-        {:error, "associated schema #{inspect queryable} does not have field `#{related_key}`"}
-      true ->
-        :ok
+    if :code.get_mode() != :embedded do
+      cond do
+        not is_atom(queryable) or queryable in env.context_modules ->
+          :ok
+        not Code.ensure_compiled?(queryable) ->
+          {:error, "associated schema #{inspect queryable} does not exist"}
+        not function_exported?(queryable, :__schema__, 2) ->
+          {:error, "associated module #{inspect queryable} is not an Ecto schema"}
+        is_nil queryable.__schema__(:type, related_key) ->
+          {:error, "associated schema #{inspect queryable} does not have field `#{related_key}`"}
+        true ->
+          :ok
+      end
     end
   end
 
@@ -819,17 +821,19 @@ defmodule Ecto.Association.BelongsTo do
 
   @doc false
   def after_compile_validation(%{queryable: queryable, related_key: related_key}, env) do
-    cond do
-      not is_atom(queryable) or queryable in env.context_modules ->
-        :ok
-      not Code.ensure_compiled?(queryable) and :code.get_mode() != :embedded ->
-        {:error, "associated schema #{inspect queryable} does not exist"}
-      not function_exported?(queryable, :__schema__, 2) ->
-        {:error, "associated module #{inspect queryable} is not an Ecto schema"}
-      is_nil queryable.__schema__(:type, related_key) ->
-        {:error, "associated schema #{inspect queryable} does not have field `#{related_key}`"}
-      true ->
-        :ok
+    if :code.get_mode() != :embedded do
+      cond do
+        not is_atom(queryable) or queryable in env.context_modules ->
+          :ok
+        not Code.ensure_compiled?(queryable) ->
+          {:error, "associated schema #{inspect queryable} does not exist"}
+        not function_exported?(queryable, :__schema__, 2) ->
+          {:error, "associated module #{inspect queryable} is not an Ecto schema"}
+        is_nil queryable.__schema__(:type, related_key) ->
+          {:error, "associated schema #{inspect queryable} does not have field `#{related_key}`"}
+        true ->
+          :ok
+      end
     end
   end
 
@@ -973,21 +977,23 @@ defmodule Ecto.Association.ManyToMany do
 
   @doc false
   def after_compile_validation(%{queryable: queryable, join_through: join_through}, env) do
-    cond do
-      not is_atom(queryable) or queryable in env.context_modules ->
-        :ok
-      not Code.ensure_compiled?(queryable) and :code.get_mode() != :embedded ->
-        {:error, "associated schema #{inspect queryable} does not exist"}
-      not function_exported?(queryable, :__schema__, 2) ->
-        {:error, "associated module #{inspect queryable} is not an Ecto schema"}
-      not is_atom(join_through) ->
-        :ok
-      not Code.ensure_compiled?(join_through) ->
-        {:error, ":join_through schema #{inspect join_through} does not exist"}
-      not function_exported?(join_through, :__schema__, 2) ->
-        {:error, ":join_through module #{inspect join_through} is not an Ecto schema"}
-      true ->
-        :ok
+    if :code.get_mode() != :embedded do
+      cond do
+        not is_atom(queryable) or queryable in env.context_modules ->
+          :ok
+        not Code.ensure_compiled?(queryable) ->
+          {:error, "associated schema #{inspect queryable} does not exist"}
+        not function_exported?(queryable, :__schema__, 2) ->
+          {:error, "associated module #{inspect queryable} is not an Ecto schema"}
+        not is_atom(join_through) ->
+          :ok
+        not Code.ensure_compiled?(join_through) ->
+          {:error, ":join_through schema #{inspect join_through} does not exist"}
+        not function_exported?(join_through, :__schema__, 2) ->
+          {:error, ":join_through module #{inspect join_through} is not an Ecto schema"}
+        true ->
+          :ok
+      end
     end
   end
 
